f7e19dee7cd95e57e69f60a7207dbf363691e7a9
Strafwerkje <66158426+Strafwerkje@users.noreply.github.com>
2020-08-05 00:03:04 +0200

    s

---
+++ commands/welkom.js
@@ -0,0 +1,33 @@
+const { RichEmbed } = require('discord.js');
+
+exports.run = async (client, message, args) => {
+
+    /*
+        1) Use the messageReactionAdd and messageReactionRemove events to add/remove users roles
+        2) Remove the awaitReactions() function as we won't need that anymore
+        3) Customize the message a bit more to fit a general welcome channel
+    */
+
+    await message.delete().catch(O_o => {});
+
+    const a = message.guild.roles.get('719936328158019585'); // Moderator
+
+    const embed = new RichEmbed()
+        .setTitle('Verify Syteem')
+        .setDescription(`
+       
+        Welkom bij **${message.guild.name}**! Laat je verifieren door op de reactie te klikken!
+       🇦 ${a.toString()}
+       `)
+        .setColor(0xdd9323)
+        .setFooter(`Guild ID: ${message.guild.id}`);
+
+    message.channel.send(embed).then(async msg => {
+
+        await msg.react('🇦');
+    });
+};
+
+exports.help = {
+    name: 'verify'
+};
